32f9db07545f207c698ee6fd85c1716d2fc6ca42
Jordan Pryde <jordan@pryde.me>
2025-01-28 11:05:21 -0800

    fix bug when there is only one kernel

--- files/system/usr/bin/regen-initramfs.sh
+++ files/system/usr/bin/regen-initramfs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -oue pipefail
+set -oxue pipefail
 
 KVER="$(rpm -q kernel-core --qf '%{version}-%{release}.%{arch}' | head -n1)"
-ls -1 --zero /usr/lib/modules | grep -vz "^$KVER$" \
+ls -1 --zero /usr/lib/modules | ( grep -vz "^$KVER$" || true ) \
     | sed -z 's/[[:space:]]*$//' \
     | xargs -r -0 -- printf "/usr/lib/modules/%s\0" \
     | xargs -r -0 -- rm -rf
